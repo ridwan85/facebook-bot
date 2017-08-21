f6814bad66bbbb60b849eb4139f6b69f33984287
unknown <muhd.azri@fiction-labs.com>
2017-08-21 13:37:27 +0800

    Changing port again

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 3000; 
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!')
+app.listen(port, function () {
+  console.log('Example app listening on port !' +port)
 })
