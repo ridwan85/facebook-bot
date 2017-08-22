b6d522fb42bfeb0acc2ae00c135fd2675d45a762
DESKTOP-5U9DE00\Fiction Labs <muhd.azri@fiction-labs.com>
2017-08-22 11:34:43 +0800

    add request package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000; 
 var bodyParser = require('body-parser')
+var request = require('request');
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
